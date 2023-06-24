12672b7b66ca3aa83dad4ed8d33d880fdae8099b
fussionace <135525415+fussionace@users.noreply.github.com>
2023-06-24 15:35:42 +0100

    Revert "Added Landing page design"

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
-import Home from './pages/unAuthPages/Home';
+import { ReactComponent as AuxureLogo } from './assets/svg/logo.svg';
 
 function App() {
-  return <Home />;
+  return (
+    <div className='w-full h-screen flex flex-col justify-center items-center gap-4 bg-background-color'>
+      <AuxureLogo className='' />
+      {/* <h1 className='font-secondary text-primary-color'> */}
+      <h1 className='text-primary-color leading-2 text-xl font-semibold uppercase font-secondary '>
+        Discover The Essence Of Timeless Luxury
+      </h1>
+    </div>
+  );
 }
 
 export default App;
